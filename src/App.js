b4743d98fd70a4148ed14499e375c3c4c0f11ae5
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-07-25 23:15:13 +0900

    ルーティング修正

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import { BrowserRouter as Router} from "react-router-dom";
 import AuthRoutes from "./routes/AuthRoutes";
 import TodoRoutes from "./routes/TodoRoutes";
 import PrivateRoute from "./routes/PrivateRoute";
-import Admin from "./Admin";
+
 
 function App() {
   return (
     <Router>
-      <Admin />
       <AuthRoutes />
       <TodoRoutes />
       <PrivateRoute />
